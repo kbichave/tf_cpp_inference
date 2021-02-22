6dfa2820e3c25e441dc55cde22d3d5262d16f9e1
Kshitij Bichave <48989717+kbichave@users.noreply.github.com>
2021-02-22 08:12:30 -0500

    Update run_inference.sh

--- run_inference.sh
+++ run_inference.sh
@@ -1,10 +1,6 @@
 mkdir build
-cp pointcloud_data/muffler_2.ply build/
-cp frozen_graph/graph.pb build/
-cp pointcloud_data/tacked_prat_clean_20_20_20_1_1.ply build/
-cp pointcloud_data/tacked_prat_noise.ply build/
 cp -r data / build/
 cd build
 cmake ..
 make
-./3DSmoothNet --fileCloud muffler_2.ply 
+./3DSmoothNet --fileCloud <path_to_point_cloud_file>
